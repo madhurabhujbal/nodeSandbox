5bd99848494180d6ac93c84bcc5eff23fd0e75cd
Madhura <madhura.bhujbal@gmail.com>
2020-04-24 12:24:24 +0100

    Modify the code to avoid redundancy

--- randomNumberGenerator.js
+++ randomNumberGenerator.js
@@ -1,26 +1,21 @@
-let num =3;
+let num = 3;
 
 let arrNum = arrayGen(num);
 console.log(arrNum);
 
-
 function arrayGen(num){
     let a = new Array(num);
     for(let i = 0;  i < a.length; i++) {
-        a[i] = randomNumGenBetween(1, 100);
-        //a[i] = randomNumGenBetween(1, 10);
+        a[i] = randomNumGenBetween(0, 100);
     }
     return a;
 };
 
 function randomNumGenBetween(min, max) {
     return Math.floor(
-        Math.random() * (max - min + 1) + min
-    )
+        (Math.random() * (max - min + 1)) + min);
 };
 
-function randomNumGenBetween(max) {
-    return Math.floor(
-        Math.random() * (max + 1)
-    )
+function randomNumGenUntil(max) {
+    return randomNumGenBetween(0, max);
 };
