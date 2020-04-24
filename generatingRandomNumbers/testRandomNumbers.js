2edceb718e01a72627d675219bf6e0cc7b4b0ba5
Madhura <madhura.bhujbal@gmail.com>
2020-04-24 19:22:23 +0100

    Create tests to check all operations work correctly

---
+++ generatingRandomNumbers/testRandomNumbers.js
@@ -0,0 +1,12 @@
+const mocha = require('mocha');
+
+//valid case tests:
+describe('Check if array of random numbers is generated', function() {
+    //1. giving 3,2,1,0 parameters
+    
+})
+
+//2. giving negetive parameters
+//3. giving any other datatype other than integer/ fraction
+
+//invalid case tests:
