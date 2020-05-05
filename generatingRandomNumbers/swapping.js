820b847c1a418f4b2edee65aa94d3605f7f96b01
Madhura <madhura.bhujbal@gmail.com>
2020-05-05 17:15:02 +0100

    Optimize the sorting logic

--- generatingRandomNumbers/swapping.js
+++ generatingRandomNumbers/swapping.js
@@ -1,17 +1,19 @@
 
     let result = [2, 5, 7, 4, 56, 45];
     console.log("Given array : " + result);
-    let sortedArray;
-    let swapped = false;
-    do{
+    let swapped = true;
+
+    while(swapped == true) {
         swapped = false;
-        for(let i = 1; i < result.length; i++) {
-            if(result[i - 1] > result[i]) {
-                let temp = result[i - 1];
-                result[i - 1] = result[i];
-                result[i] = temp;
-                swapped = true;
+        //for(let j = 1; j <= result.length; j++) {
+            for(let i = 1; i < result.length; i++) {
+                if(result[i - 1] > result[i]) {
+                    let temp = result[i - 1];
+                    result[i - 1] = result[i];
+                    result[i] = temp;
+                    swapped = true;
+                }
+                console.log("Sorted Array : " + result);
             }
-        }
-        console.log("Sorted Array : " + result);
-    } while(swapped == true);
+       // }
+    }
