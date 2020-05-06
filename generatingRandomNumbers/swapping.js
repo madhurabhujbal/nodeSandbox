68652ad63939e3cfc3256e3561230f25d7ab5bba
Madhura <madhura.bhujbal@gmail.com>
2020-05-06 16:22:34 +0100

    Revise swapping logic

--- generatingRandomNumbers/swapping.js
+++ generatingRandomNumbers/swapping.js
@@ -1,19 +1,23 @@
 
-    let result = [2, 5, 7, 4, 56, 45];
+    let result = [32, 5, 7, 4, 1, 45];
     console.log("Given array : " + result);
     let swapped = true;
+    let count = 0;
 
     while(swapped == true) {
         swapped = false;
         //for(let j = 1; j <= result.length; j++) {
-            for(let i = 1; i < result.length; i++) {
+            for(let i = 1; i < result.length - count; i++) {
                 if(result[i - 1] > result[i]) {
                     let temp = result[i - 1];
                     result[i - 1] = result[i];
                     result[i] = temp;
                     swapped = true;
                 }
-                console.log("Sorted Array : " + result);
             }
-       // }
-    }
+            count++;
+            // }
+        }
+    console.log("Sorted Array : " + result);
+
+
