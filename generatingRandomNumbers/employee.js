2456fb593bd461a3168b10563e3bca3aa7155ccf
Madhura <madhura.bhujbal@gmail.com>
2020-05-09 12:03:51 +0100

    Write code for sorting employee by id

---
+++ generatingRandomNumbers/employee.js
@@ -0,0 +1,28 @@
+
+
+function getSortedById(empArray) {
+    let swapped = true;
+    //continue until no more swapping
+    while(swapped == true) {
+        swapped = false;
+        //check for all elements
+        for(let i = 0; i < empArray.length - 1; i++) {
+            //if current is greater than next
+            if(empArray[i]['id'] > empArray[i + 1]['id']) {
+                //swap the elements
+                let temp = empArray[i];
+                empArray[i] = empArray[i + 1];
+                empArray[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    }
+}
+
+let empArray = [{name: "Madhura", id : 28, age : 19},
+                {name: "Tom", id : 08, age : 13},
+                {name: "Jerry", id : 59, age : 23},
+                {name: "Ben", id : 24, age : 33}];
+console.log(empArray);
+getSortedById(empArray);
+console.log(empArray);
