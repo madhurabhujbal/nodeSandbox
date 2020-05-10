e307a16eff1c78dd6173c4d5be3c7ef6a1751c7d
Madhura <madhura.bhujbal@gmail.com>
2020-05-10 23:14:49 +0100

    Write code for inserting a node at end of list

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ function addListItem(head, num) {
     return head;
 }
 
-// const head = {value: 89, next: undefined};
-// const node1 = {value: 45, next: undefined};
-// head.next = node1;
-// const node2 = {value: 36, next: undefined};
-// node1.next = node2;
-let head = undefined;
+let head = {value: 89, next: undefined};
+const node1 = {value: 45, next: undefined};
+head.next = node1;
+const node2 = {value: 36, next: undefined};
+node1.next = node2;
+//let head = undefined;
 
 console.log("Before : ");
 printList(head);
