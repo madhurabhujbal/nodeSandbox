882b14f1961db815e24425e4cdc89c87fef2d532
Madhura <madhura.bhujbal@gmail.com>
2020-05-10 23:12:31 +0100

    Write code for inserting a node if list is empty

--- app.js
+++ app.js
@@ -7,10 +7,30 @@ function printList(head) {
     }
 }
 
-const head = {value: 89, next: undefined};
-const node1 = {value: 45, next: undefined};
-head.next = node1;
-const node2 = {value: 36, next: undefined};
-node1.next = node2;
+function addListItem(head, num) {
+    if(head == undefined) {
+        head = {value: num, next: undefined};
+        return head;
+    }
 
+    let current = head;
+    while(current.next != undefined) {
+        current = current.next;
+    }
+    let node3 = {value: num, next: undefined};
+    current.next = node3;
+    return head;
+}
+
+// const head = {value: 89, next: undefined};
+// const node1 = {value: 45, next: undefined};
+// head.next = node1;
+// const node2 = {value: 36, next: undefined};
+// node1.next = node2;
+let head = undefined;
+
+console.log("Before : ");
+printList(head);
+head = addListItem(head, 67);
+console.log("After : ");
 printList(head);
