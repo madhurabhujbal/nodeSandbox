f62711d8b75e1abb88ff40665dc49616c8e91587
Madhura <madhura.bhujbal@gmail.com>
2020-05-11 12:35:07 +0100

    Add code to check if element is present in list and delete list item

--- app.js
+++ app.js
@@ -22,15 +22,41 @@ function addListItem(head, num) {
     return head;
 }
 
+function deleteListItem(head, number) {
+    let current = head;
+    let flag = false;
+    for(current = head; current != undefined; current = current.next) {
+        if(current.value == number) {
+            flag = true;
+            break;
+        }
+    }
+
+    if(flag == false) {
+        return "Element not present in the list";
+    }
+
+    while(current.next.value != number) {
+        current = current.next;
+    }
+    current.next = current.next.next;
+}
+
 let head = {value: 89, next: undefined};
-const node1 = {value: 45, next: undefined};
+let node1 = {value: 45, next: undefined};
 head.next = node1;
-const node2 = {value: 36, next: undefined};
+let node2 = {value: 36, next: undefined};
 node1.next = node2;
 //let head = undefined;
 
-console.log("Before : ");
+console.log("Before adding list item : ");
 printList(head);
-head = addListItem(head, 67);
-console.log("After : ");
+head = addListItem(head, 79);
+head = addListItem(head, 77);
+console.log("After adding list item : ");
 printList(head);
+let result = deleteListItem(head, 39);
+console.log(result);
+console.log("After deleting list item : ");
+printList(head);
+
