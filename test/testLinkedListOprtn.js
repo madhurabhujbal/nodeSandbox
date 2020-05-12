4264929b76abdd62e37651c11051802abf52c653
Madhura <madhura.bhujbal@gmail.com>
2020-05-12 23:44:35 +0100

    Write and run test if position exceeds length of linked-list

--- test/testLinkedListOprtn.js
+++ test/testLinkedListOprtn.js
@@ -1,9 +1,34 @@
 const assert = require('chai').assert;
-const {printList, addListItem, deleteListItem} = require('../linkedListOperations');
+const {addListItem, addItemInMiddle, deleteListItem} = require('../linkedListOperations');
 
-describe("With an empty list", function() {
-    it("Element cannot be serched", function() {
-        let result = deleteListItem(undefined, 78);
-        assert.equal(result, "List is empty");
-    })
+
+describe("With an empty linked-list", function() {
+    it("Element passed cannot be serched or deleted", function() {
+        let head = deleteListItem(undefined, 78);
+        assert.equal(head, "List is empty");
+    });
+
+    it("Element passed would be added as head in list", function() {
+        let head = addListItem(undefined, 45);
+        assert.equal(head.value, 45);
+        assert.equal(head.next, undefined);
+    });
 });
+
+describe("If position of element to be inserted exceeds linked-list length", function() {
+    it("Should give a warning message", function() {
+        let head = undefined;
+        head = addListItem(head, 89);
+        head = addListItem(head, 819);
+        head = addItemInMiddle(head, 5, 34);
+        // assert.equal(head, " ");
+    });
+});
+
+// describe("By passing element that is not present in linked-list", function() {
+//     it("Searching element should return a warning message", function() {
+//         // let head = deleteListItem({head: {value: 87, next: undefined}, node1: {value: 56, next: undefined}}, 99);
+//         // assert.equal(head, "Element not present in the list");
+
+//     });
+// });
