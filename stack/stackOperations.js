d65f6d51e9b470e3fa3e7f78885b09d29712024e
Madhura <madhura.bhujbal@gmail.com>
2020-05-17 16:02:23 +0100

    add function to pop element from stack

--- stack/stackOperations.js
+++ stack/stackOperations.js
@@ -1,17 +1,29 @@
 
 let stack = [];
 
-push(9);
-push(17);
-push(344);
-push(236);
-
 function push(number) {
     let num = number
     stack.push(num);
-    printStack(stack);
 }
 
 function printStack(stack) {
     console.log(stack);
 }
+
+function pop() {
+    let top = stack.length;
+    if(top == 0) {
+        return "Stack underflow error";
+    }
+    top = top - 1;
+    return stack.pop();
+}
+
+push(9);
+push(17);
+push(344);
+push(236);
+printStack(stack);
+pop();
+printStack(stack);
+
