3fa37f7f4e2e2fb906ee691037d3079b2e0a591e
Madhura <madhura.bhujbal@gmail.com>
2020-05-18 11:35:36 +0100

    Create a queue, add elements in it and print queue

---
+++ queue/queueOperations.js
@@ -0,0 +1,17 @@
+
+//Queue operations: enqueue, dequeue, print,
+
+// function enqueue(Queue, number) {
+
+// }
+
+function printQueue(Queue) {
+    console.log(Queue);
+}
+
+let Queue = [];
+Queue.push(8);
+Queue.push(238);
+Queue.push(84);
+Queue.push(78);
+printQueue(Queue);
