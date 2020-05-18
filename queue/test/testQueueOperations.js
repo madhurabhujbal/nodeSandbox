dfa2b497862d6fae0f463b1ebc82101e5bc39ba1
Madhura <madhura.bhujbal@gmail.com>
2020-05-18 12:46:32 +0100

    Test enqueue() and dequeue() for non-empty queue

--- queue/test/testQueueOperations.js
+++ queue/test/testQueueOperations.js
@@ -13,3 +13,16 @@ describe("for empty queue : ", function() {
         assert.equal(Queue, 61);
     });
 });
+
+describe("for non-empty queue : ", function() {
+    let Queue = [45, 78, 33, 58, 136, 247];
+    it("Should return first entered element in queue for dequeue()", function() {
+        let firstElement = dequeue(Queue);
+        assert.equal(firstElement, 45);
+    });
+
+    it("Should add element to top of queue for enqueue()", function() {
+        enqueue(Queue, 21);
+        assert.equal(Queue[Queue.length - 1], 21);
+    });
+});
