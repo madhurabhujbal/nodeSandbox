f85ee36dded6a72b46acac17385b70a9c9ce67c2
Madhura <madhura.bhujbal@gmail.com>
2020-05-18 11:59:43 +0100

    Add dequeue functionaliy to remove first element in queue

--- queue/queueOperations.js
+++ queue/queueOperations.js
@@ -6,6 +6,13 @@ function enqueue(Queue, number) {
     return Queue;
 }
 
+function dequeue(Queue) {
+    if(Queue.length == 0) {
+        console.log("Queue is empty");
+    }
+    return Queue.shift();
+}
+
 function printQueue(Queue) {
     console.log(Queue);
 }
@@ -13,4 +20,6 @@ function printQueue(Queue) {
 let Queue = [8, 234, 84, 78];
 printQueue(Queue);
 enqueue(Queue, 99);
+printQueue(Queue);
+dequeue(Queue);
 printQueue(Queue);
