4432b75e9a30a63289b9bbb4cc2431f1ead2db42
Madhura <madhura.bhujbal@gmail.com>
2020-05-19 16:08:13 +0100

    Find record starting with specific Letter

--- mongodbPractice/mongodbOperations.js
+++ mongodbPractice/mongodbOperations.js
@@ -6,20 +6,21 @@ MongoClient.connect(url, function(err, db) {
     if(err) throw err;
     //console.log("Database created!!");
     var dbo = db.db("mydatabase");
-    var myobject = [{name: "My Company", address: "Greater Manchester"},
-                    {name: "My Company1", address: "Greater Manchester1"},
-                    {name: "My Company2", address: "Greater Manchester2"},
-                    {name: "My Company3", address: "Greater Manchester3"},
-                    {name: "My Company4", address: "Greater Manchester4"}];
+    var myobject = [{name: "My Company", address: "Manchester"},
+                    {name: "My Company1", address: "London"},
+                    {name: "My Company2", address: "York"},
+                    {name: "My Company3", address: "India"},
+                    {name: "My Company4", address: "Leeds"}];
     dbo.createCollection("customers", function(err, res) {
     if(err) throw err;
         //console.log("Collection created!!");
-    //dbo.collection("customers").insertMany(myobject, function(err, res) {
+    dbo.collection("customers").insertMany(myobject, function(err, res) {
     //dbo.collection("customers").find({}, { projection: { _id:0 }, sort}).toArray(function(err, res) {
-    dbo.collection("customers").find({address: "Greater Manchester"}, {projection: {_id: 0, address: 0}}).toArray(function(err, res) {
+    dbo.collection("customers").find({address: /^Y/ }, {projection: {_id: 0}}).toArray(function(err, res) {
         if(err) throw err;
         console.log(res);
         db.close();
         });
     });
+});
 });
