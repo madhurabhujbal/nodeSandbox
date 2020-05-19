84b7b2483e7b3bc318f6ece39d5cd8d7509e3772
Madhura <madhura.bhujbal@gmail.com>
2020-05-19 15:38:03 +0100

    Search a record by adding search field as query object in find

--- mongodbPractice/mongodbOperations.js
+++ mongodbPractice/mongodbOperations.js
@@ -15,7 +15,8 @@ MongoClient.connect(url, function(err, db) {
     if(err) throw err;
         //console.log("Collection created!!");
     //dbo.collection("customers").insertMany(myobject, function(err, res) {
-    dbo.collection("customers").find({}, { projection: { _id:0 }}).toArray(function(err, res) {
+    //dbo.collection("customers").find({}, { projection: { _id:0 }, sort}).toArray(function(err, res) {
+    dbo.collection("customers").find({address: "Greater Manchester"}, {projection: {_id: 0, address: 0}}).toArray(function(err, res) {
         if(err) throw err;
         console.log(res);
         db.close();
