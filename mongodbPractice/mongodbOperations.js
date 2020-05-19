415b0f932331303d5ce96121189293e041f2da3c
Madhura <madhura.bhujbal@gmail.com>
2020-05-19 14:24:46 +0100

    Insert many documents in collection and search for record

--- mongodbPractice/mongodbOperations.js
+++ mongodbPractice/mongodbOperations.js
@@ -6,14 +6,19 @@ MongoClient.connect(url, function(err, db) {
     if(err) throw err;
     //console.log("Database created!!");
     var dbo = db.db("mydatabase");
-    var myobject = {name: "My Company", address: "Greater Manchester"};
-    // dbo.createCollection("customers", function(err, res) {
-    // if(err) throw err;
-    //     console.log("Collection created!!");
-    //dbo.collection("customers").insertOne(myobject, function(err, res) {
-    dbo.collection("customers").findOne({}, function(err, res) {
+    var myobject = [{name: "My Company", address: "Greater Manchester"},
+                    {name: "My Company1", address: "Greater Manchester1"},
+                    {name: "My Company2", address: "Greater Manchester2"},
+                    {name: "My Company3", address: "Greater Manchester3"},
+                    {name: "My Company4", address: "Greater Manchester4"}];
+    dbo.createCollection("customers", function(err, res) {
+    if(err) throw err;
+        //console.log("Collection created!!");
+    //dbo.collection("customers").insertMany(myobject, function(err, res) {
+    dbo.collection("customers").find({}).toArray(function(err, res) {
         if(err) throw err;
         console.log(res);
         db.close();
+        });
     });
 });
