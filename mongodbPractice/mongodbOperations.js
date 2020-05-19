0df74998c954ad53f3114e9207d230ab4d54372e
Madhura <madhura.bhujbal@gmail.com>
2020-05-19 16:52:00 +0100

    Delete one and multiple records based on criterion

--- mongodbPractice/mongodbOperations.js
+++ mongodbPractice/mongodbOperations.js
@@ -6,22 +6,9 @@ MongoClient.connect(url, function(err, db) {
     if(err) throw err;
     //console.log("Database created!!");
     var dbo = db.db("mydatabase");
-    var myobject = [{name: "My Company", address: "Manchester"},
-                    {name: "My Company1", address: "London"},
-                    {name: "My Company2", address: "York"},
-                    {name: "My Company3", address: "India"},
-                    {name: "My Company4", address: "Leeds"}];
-    dbo.createCollection("customers", function(err, res) {
-    if(err) throw err;
-        //console.log("Collection created!!");
-    dbo.collection("customers").insertMany(myobject, function(err, res) {
-    //dbo.collection("customers").find({}, { projection: { _id:0 }, sort}).toArray(function(err, res) {
-    //dbo.collection("customers").find({_id: /^0/ }, {projection: {name: 0}}).toArray(function(err, res) {
-    dbo.collection("customers").find({}, {projection: {_id: 0}}).sort({address: 1}).toArray(function(err, res) {
+    dbo.collection("customers").deleteMany({name: "My Company3"}, function(err, res) {
         if(err) throw err;
         console.log(res);
         db.close();
         });
     });
-});
-});
