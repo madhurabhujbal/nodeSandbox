d37229d12f138bb3ebd0b9baf458e75b518cbc89
Madhura <madhura.bhujbal@gmail.com>
2021-03-06 13:26:54 +0000

    Add code to check if string is a palindrome

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,5 +1,14 @@
 function checkIfPalindrome(name){
 
+    let iterations = name.length / 2;
+    for (let i = 0; i < iterations; i++) {
+        if (name[i] === name[(name.length - i) - 1]) {
+            continue;
+        } 
+        else {
+            return false;
+        }
+    }
     return true;
 }
 
