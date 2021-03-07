dcc7bfe34c7f2b560884e61ace0bbe3dd1981cb9
Madhura <madhura.bhujbal@gmail.com>
2021-03-07 19:09:18 +0000

    Code for multiplication of two numbers

--- mathOperations/app.js
+++ mathOperations/app.js
@@ -1,9 +1,12 @@
-const {add, subtract} = require('./mathOperations');
+const {add, subtract, multiply} = require('./mathOperations');
 
 let addition = add(10, 2);
 console.log(addition);
 
 let subtraction = subtract(10, 2);
 console.log(subtraction);
+
+let multiplication = multiply(10, 2);
+console.log(multiplication);
 
 
--- mathOperations/mathOperations.js
+++ mathOperations/mathOperations.js
@@ -4,10 +4,17 @@
         let addResult = num1 + num2;
         return addResult;
     }
+
     function subtract(num1, num2) {
         console.log(`Subtraction of ${num1} and ${num2} is:`);
         let subtractResult = num1 - num2;
         return subtractResult;
     }
 
-module.exports = {add, subtract};
+    function multiply(num1, num2) {
+        console.log(`Multiplication of ${num1} and ${num2} is:`);
+        let multiplicationResult = num1 * num2;
+        return multiplicationResult;
+    }
+
+module.exports = {add, subtract, multiply};
