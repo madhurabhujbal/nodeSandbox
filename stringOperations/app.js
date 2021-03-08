c2b164de676b390a191fec9ba1538e8db84a1264
Madhura <madhura.bhujbal@gmail.com>
2021-03-08 10:45:38 +0000

    Handle empty strings

--- stringOperations/app.js
+++ stringOperations/app.js
@@ -1,5 +1,5 @@
 const stringOperations = require('./stringOperations');
 
-let givenString;
+let givenString = '';
 let stringLength = stringOperations.calculateStringLength(givenString);
 console.log(`Length of given string ${givenString} is ${stringLength}`);
--- stringOperations/stringOperations.js
+++ stringOperations/stringOperations.js
@@ -2,6 +2,11 @@ function calculateStringLength(givenString) {
     if(givenString == undefined) {
         return `undefined`;
     }
+
+    else if(givenString == '') {
+        return `empty string`;
+    }
+    
     console.log(givenString[0]);
     let counter = 1;
     return 1;
