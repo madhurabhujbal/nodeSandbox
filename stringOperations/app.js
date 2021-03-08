aeb5854a3c53aff968023d9346fea57772753aeb
Madhura <madhura.bhujbal@gmail.com>
2021-03-08 10:30:42 +0000

    Add boiler plate code for performing string operations

---
+++ stringOperations/app.js
@@ -0,0 +1,5 @@
+const stringOperations = require('./stringOperations');
+
+let givenString = 'Manchester';
+let stringLength = stringOperations.calculateStringLength(givenString);
+console.log(`Length of given string ${givenString} is ${stringLength}`);
---
+++ stringOperations/stringOperations.js
@@ -0,0 +1,5 @@
+function calculateStringLength(givenString) {
+    return `User given string ${givenString} recieved`;
+}
+
+module.exports = {calculateStringLength}
