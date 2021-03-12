1d112ea2b5eef803327837e714951286de068132
Madhura <madhura.bhujbal@gmail.com>
2021-03-12 12:03:38 +0000

    Generate pattern using for loop

--- patternMaker/main.js
+++ patternMaker/main.js
@@ -1,9 +1,11 @@
 
 let num = 3;
 
-for(let i = 1; i <= num; i++) {
-    console.log('*'.repeat(i));
-}
-for(let i = num - 1; i >= 1; i--) {
-    console.log('*'.repeat(i));
+for(let i = 0; i < num; i++) {
+    //print i+1 times
+    let myString = "";
+    for(j = 0; j < i + 1; j++) {
+        myString += "*";
+    }
+    console.log(myString);
 }
