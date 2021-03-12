39fbb6c4348856f5f4917c1bc64d38be2b1419a9
Madhura <madhura.bhujbal@gmail.com>
2021-03-12 17:31:50 +0000

    Add function to calculate count of duplicate nos in result array

--- randomNumberGenerator/randomNumberGenerator.js
+++ randomNumberGenerator/randomNumberGenerator.js
@@ -1,9 +1,12 @@
 
-let minValue = 1;
-let maxValue = 100;
-let numbersInArray = 5;
-let resultArray = getRandomNumberArray(numbersInArray, minValue, maxValue);
-console.log(resultArray);
+// let minValue = 1;
+// let maxValue = 100;
+// let numbersInArray = 5;
+// let resultArray = getRandomNumberArray(numbersInArray, minValue, maxValue);
+// console.log(resultArray);
+let resultArray = [1, 2, 1, 3, 1];
+let duplicateCount = findDuplicates(resultArray);
+console.log(duplicateCount);
 
 function getRandomNumberArray(numbersInArray, minValue, maxValue) {
     let randomNumberArray = [];
@@ -19,4 +22,20 @@ function generateRandomNumber(minValue, maxValue) {
     minValue = Math.ceil(minValue);
     maxValue = Math.floor(maxValue);
     return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+}
+
+function findDuplicates(resultArray) {
+    let count = 0;
+    for(let i = 0; i < resultArray.length; i++) {
+        for(let j = 0; j < resultArray.length; j++) {
+            if(j == i) {
+                continue;
+            }
+            
+            if(resultArray[i] == resultArray[j]) {
+                count = count + 1;
+            }
+        }
+    }
+    return count;
 }
