21f791b7567815d647d8bf02a898b0674123e07d
Madhura <madhura.bhujbal@gmail.com>
2021-03-16 11:46:07 +0000

    Code to repeat the pattern to create a diamond of given size

--- patternMaker/main.js
+++ patternMaker/main.js
@@ -1,5 +1,5 @@
 
-let num = 4;
+let num = 20;
 let pattern = patternMaker(num);
 for(let i = 0; i < pattern.length; i++) {
     console.log(pattern[i]);
@@ -11,10 +11,22 @@ function patternMaker(num) {
     }
 
     let pattern = [];
-    for(let i = 1, j = 4; i <= num, j > 0; i++, j--) {
+    for(let i = 1, j = num; i <= num, j > 0; i++, j--) {
         let spaces = "";
         let stars = "";
         for(let s = 0; s < (j - 2) + 1; s++) {
+            spaces += " ";
+        }
+        for(let r = 0; r < (i * 2) - 1; r++) {
+            stars += "*";
+        }
+        pattern.push(spaces + stars)
+    }
+
+    for(let i = num - 1, j = 1; i > 0, j < num; i--, j++) {
+        let spaces = "";
+        let stars = "";
+        for(let s = 0; s <= (j - 2) + 1; s++) {
             spaces += " ";
         }
         for(let r = 0; r < (i * 2) - 1; r++) {
