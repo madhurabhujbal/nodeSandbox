7243beabd94f11cc1b9539229394b1e2f5d06796
Madhura <madhura.bhujbal@gmail.com>
2021-03-21 19:17:29 +0000

    Add boilerplate code for the problem

---
+++ sumPair/sum.js
@@ -0,0 +1,16 @@
+function getPair(input, targetSum) {
+    // Scan the input array and return a pair that matches target sum, empty array for no match
+
+    return [];
+}
+
+function getInput(fileName) {
+    // Read input data from the given file
+    return [ 1721, 979, 366, 299, 675, 1456];
+}
+
+let sampleInput = getInput("smallInput.txt");
+let targetSum = 2020;
+
+let result = getPair(sampleInput, targetSum);
+console.log("Result pair: ", result);
