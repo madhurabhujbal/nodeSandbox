d1d9e032d7d6cd5281d1c1994b6ca8ffee59135c
Madhura <madhura.bhujbal@gmail.com>
2021-03-21 19:33:27 +0000

    Add tests for getPair function

---
+++ sumPair/test/sumTest.js
@@ -0,0 +1,16 @@
+let sum = require('../sum');
+const assert = require('chai').assert;
+
+describe('If a pair with given target sum exists', function() {
+    let input = [ 1721, 979, 366, 299, 675, 1456];
+    let targetSum = 2020;
+    let resultArray= sum.getPair(input, targetSum);
+    it('Should return the pair', function() {
+        assert.equal(resultArray.length, 2);
+    });
+
+    it('Should return the pair that adds up to target sum', function() {
+        let resultArray= sum.getPair(input, targetSum);
+        assert.deepEqual(resultArray[0] + resultArray[1], targetSum);
+    });
+});
