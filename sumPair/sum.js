d3aea8b9e06ecc9f388332d8d55e92fd95a93937
Madhura <madhura.bhujbal@gmail.com>
2021-03-22 11:49:34 +0000

    Add a naive approach with complexity O(n^2)

--- sumPair/sum.js
+++ sumPair/sum.js
@@ -1,7 +1,19 @@
 function getPair(input, targetSum) {
     // Scan the input array and return a pair that matches target sum, empty array for no match
-
-    return [];
+    let resultArray = [];
+    for (let i = 0; i < input.length; i++) {
+        for(let j = 0; j < input.length; j++) {
+            if (i == j) {
+                continue;
+            }
+            if(input[i] + input[j] === targetSum) {
+                resultArray.push(input[i]);
+                resultArray.push(input[j]);
+                return resultArray;
+            }
+        }
+    }
+    return resultArray;
 }
 
 function getInput(fileName) {
