00d4e6e839afe3165ced606cb24306d95f12b324
Madhura <madhura.bhujbal@gmail.com>
2021-03-22 14:48:45 +0000

    Test when no pair matches target sum

--- sumPair/test/sumTest.js
+++ sumPair/test/sumTest.js
@@ -14,3 +14,12 @@ describe('If a pair with given target sum exists', function() {
         assert.deepEqual(resultArray[0] + resultArray[1], targetSum);
     });
 });
+
+describe('If no pair of numbers matches target sum', function() {
+    let input = [ 1721, 979, 366, 29, 675, 1456];
+    let targetSum = 2020;
+    let resultArray = sum.getPair(input, targetSum);
+    it('Should return an empty array', function() {
+        assert.deepEqual(resultArray, []);
+    });
+});
