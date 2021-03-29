7262c1517933e9a1bc990c06bc6935e43bab9c38
Madhura <madhura.bhujbal@gmail.com>
2021-03-29 16:38:53 +0100

    Seperate the functions for password validation

--- passwordValidation/passwordValidation.js
+++ passwordValidation/passwordValidation.js
@@ -1,39 +1,52 @@
 const fs = require('fs');
 
 function processRecord(line) {
+    let dataObject = {};
     let cols = line.split(" ");
 
     // Get limits from the first column
     let lowerLimit = parseInt(cols[0].split('-')[0]);
+    dataObject['lowerLimit'] = lowerLimit;
     let upperLimit = parseInt(cols[0].split('-')[1]);
+    dataObject['upperLimit'] = upperLimit;
 
     // Get char from the second column
     let charToMatch = cols[1][0];
+    dataObject['charToMatch'] = charToMatch;
 
     // Get input string from the third column
     let password = cols[2];
-    console.log(password);
+    dataObject['password'] = password;
+    return dataObject;
 
-    if(password[lowerLimit - 1] == charToMatch && password[upperLimit - 1] != charToMatch) {
+}
+
+function passwordValidation2(recordData) {
+    let password = recordData.password;
+    if(password[recordData.lowerLimit - 1] == recordData.charToMatch && password[recordData.upperLimit - 1] != recordData.charToMatch) {
         return true;
     }
 
-    if(password[lowerLimit - 1] != charToMatch && password[upperLimit - 1] == charToMatch) {
+    if(password[recordData.lowerLimit - 1] != recordData.charToMatch && password[recordData.upperLimit - 1] == recordData.charToMatch) {
         return true;
     }
-
-    // let count = 0;
-    // for(let i = 0; i < password.length; i++){
-    //     if(password[i] == charToMatch) {
-    //         count++;
-    //     }
-    // }
-    // if(count >= lowerLimit && count <= upperLimit) {
-    //     console.log(line);
-    //     return true;
-    // }
     return false;
 }
+
+function passwordValidation1(recordData) {
+    let password = recordData.password;
+    let count = 0;
+    for(let i = 0; i < password.length; i++){
+        if(password[i] == recordData.charToMatch) {
+            count++;
+        }
+    }
+    if(count >= recordData.lowerLimit && count <= recordData.upperLimit) {
+        return true;
+    }
+    return false;
+}
+
 
 function processInput(fileName) {
     let result = 0;
@@ -41,7 +54,8 @@ function processInput(fileName) {
         const data = fs.readFileSync(fileName, 'utf-8');
         const lines = data.split(/\r?\n/);
         for(let i = 0; i < lines.length; i++) {
-            if(processRecord(lines[i])) {
+            let recordData = processRecord(lines[i]);
+            if(passwordValidation2(recordData)) {
                 result++;
             }
         }
